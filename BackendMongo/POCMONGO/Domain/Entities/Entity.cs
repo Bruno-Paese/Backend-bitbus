f0a8108434f60f61b08d36163e679cb6707cdba2
Bruno-Paese <123875972+Bruno-Paese@users.noreply.github.com>
2024-05-09 20:58:53 -0300

    Carga inicial

---
+++ BackendMongo/POCMONGO/Domain/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using POCMONGO.Domain.Entities;
+
+namespace POC_Mongo.Src.Domain.Entities
+{
+    public abstract class Entity
+    {
+
+        protected static String DATABASE = "curso-node";
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+    }
+}
