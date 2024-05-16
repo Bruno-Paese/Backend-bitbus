2ee472f15b425996935254e3661d13292bcbd714
Bruno-Paese <123875972+Bruno-Paese@users.noreply.github.com>
2024-05-16 20:46:33 -0300

    Backend de visitas

---
+++ BackendMongo/POCMONGO/Controllers/VisitController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POCMONGO.Domain.Entities;
+
+namespace ItemStoreApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VisitController : ControllerBase
+{
+    [HttpGet]
+    public async Task<List<Visit>> Get() =>
+        await (new Visit()).getAll();
+
+    [HttpGet("{id}")]
+    public async Task<Visit> GetOne(string id) =>
+        await (new Visit()).getOne(id);
+
+    [HttpPost]
+    public async Task<IActionResult> save([FromBody] Visit visit)
+    {
+        if (await visit.save())
+        {
+            return CreatedAtAction("Save", visit);
+        }
+
+        return BadRequest();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> update([FromBody] Visit visit)
+    {
+        if (await visit.update())
+        {
+            return CreatedAtAction("Update", visit);
+        }
+
+        return BadRequest();
+    }
+}
---
+++ BackendMongo/POCMONGO/Domain/Entities/Visit.cs
@@ -0,0 +1,86 @@
+﻿using MongoDB.Driver;
+using POC_Mongo.Src.Domain.Entities;
+using POC_Mongo.Src.Repositories.MongoDB;
+
+namespace POCMONGO.Domain.Entities
+{
+    public class Visit : Entity, IEntity
+    {
+        private const string COLLECTION_NAME = "Visit";
+
+        private IMongoCollection<Visit> collection;
+
+        public String local {  get; set; }
+        public String period {  get; set; }
+        public String responsable {  get; set; }
+        public String[] visitors {  get; set; }
+        public Item[] items { get; set; }
+
+
+        public Visit()
+        {
+            MongoClient client = MongoDBRepository.connect();
+            collection = client.GetDatabase(DATABASE).GetCollection<Visit>(COLLECTION_NAME);
+        }
+
+        public async Task<List<Visit>> getAll()
+        {
+            List<Visit> items = await collection.Find(_ => true).ToListAsync();
+            return items;
+        }
+
+        public async Task<Visit> getOne(string id)
+        {
+            return await collection.Find((x) => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Boolean> save()
+        {
+            try
+            {
+                await collection.InsertOneAsync(this);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<Boolean> update()
+        {
+            try
+            {
+                await collection.ReplaceOneAsync<Visit>((x) => x.Id == Id, this);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<Boolean> delete()
+        {
+            try
+            {
+                await collection.DeleteOneAsync<Visit>((x) => x.Id == Id);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        Task<List<IEntity>> IEntity.getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEntity> IEntity.getOne(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
