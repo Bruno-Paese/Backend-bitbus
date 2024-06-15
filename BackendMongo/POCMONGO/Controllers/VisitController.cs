95f8b894a731891108c984aef33fd857fedd7c58
Bruno-Paese <123875972+Bruno-Paese@users.noreply.github.com>
2024-06-15 10:52:59 -0300

    Atualiza retorno para retornar visitante

--- BackendMongo/POCMONGO/Controllers/VisitController.cs
+++ BackendMongo/POCMONGO/Controllers/VisitController.cs
@@ -141,7 +141,7 @@ public class VisitController : ControllerBase
 
             if (await visit.update())
             {
-                return CreatedAtAction("Update", visit);
+                return CreatedAtAction("Update", visitor);
             }
 
             return BadRequest();
